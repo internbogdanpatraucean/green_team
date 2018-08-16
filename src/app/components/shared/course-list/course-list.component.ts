2e3c48ade95af79cf79dd13e60653108b1c2279d
BulubenchiIosif <intern.iosif.bulubenchi@assist.ro>
2018-08-16 17:41:42 +0300

    [UI] Modified courses-list interface and logics

--- src/app/components/shared/course-list/course-list.component.ts
+++ src/app/components/shared/course-list/course-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { splitClasses } from '@angular/compiler';
 
 @Component({
   selector: 'app-course-list',
@@ -7,16 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseListComponent implements OnInit {
   // public var numeCurs:string ='java';
-  constructor() { }
+  categories = ['Astrology',
+    'Economy',
+    'Finance',
+    'Grammar',
+    'Fun Facts',
+    'Jokes',
+    'Life Hacks',
+    'Games',
+    'Matematics',
+    'ddddd',
+    'sfsfs',
+    'sdsaf',
+    'sfafsa',
+    'sfafa'
+  ]
+  
+  newCategories = this.categories.slice(0,6);
+  searchName = '';
+  index=6;
 
   ngOnInit() {
   }
-  onClickSearch()
-  {
-    console.log("java");
+  
+  onClickSearch() {
+    this.newCategories =[];
+    for( var i=0;i<this.categories.length;i++){
+      if(this.searchName!="" && this.categories[i].toLowerCase().indexOf(this.searchName.toLowerCase()) != -1){
+        this.newCategories.push(this.categories[i]);
+        console.log("Exista");
+      }
+    }
   }
-  onClickDiscover()
-  {
-    console.log("java2");
+
+
+  onClickDiscover() {
+    
+    this.newCategories = this.categories.slice(this.index,this.index+6);
+    this.index+=6;
+    this.searchName="";
+    if(this.categories.indexOf===)
+
+    }
   }
+
+
+
+
 }
